2d47323430056f766d272592f4ad340f7a3d3c82
Owen Dearman <owendearman@gmail.com>
2022-06-09 14:23:22 +0100

    removed unnecessary information from definition and synonym functions

--- src/addDefinitionsToWords.ts
+++ src/addDefinitionsToWords.ts
@@ -4,7 +4,7 @@ import { fullwordData } from "./interfaces";
 export async function addDefinitionsToWords(client: Client, wordList: fullwordData[]) {
     for (const word of wordList) {
         try {
-            const definitionRes = await client.query(`select * from meanings where word_id = $1`, [word.id])
+            const definitionRes = await client.query(`select id, meaning, pos from meanings where word_id = $1`, [word.id])
             word.meanings = definitionRes.rows
         } catch (error) {
             console.log(error)
--- src/addSynonymsToWords.ts
+++ src/addSynonymsToWords.ts
@@ -4,7 +4,7 @@ import { Client } from "pg";
 export async function addSynonymsToWords(client: Client, wordList: fullwordData[]) {
     for (const word of wordList) {
         try {
-            const synonymRes = await client.query(`select * from synonyms where word_id = $1`, [word.id])
+            const synonymRes = await client.query(`select id, word from synonyms where word_id = $1`, [word.id])
             word.synonyms = synonymRes.rows
         } catch (error) {
             console.log(error)
